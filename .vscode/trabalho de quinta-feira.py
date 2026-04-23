0703cb07cdbc81d4a8dc0482f989161e6eb24f8f
nathanielmendes02-lab <nathanielmendes02@gmail.com>
2026-04-23 08:33:42 -0300

    Update trabalho de quinta-feira.py

--- .vscode/trabalho de quinta-feira.py
+++ .vscode/trabalho de quinta-feira.py
@@ -1,17 +1,43 @@
-for i in range(1, 5):
-    x = input(f"Digite o {i}º número: ")
-    
-    while not x.isdigit():
-        print("Erro: digite apenas números inteiros positivos.") 
-        x = input(f"Digite o {i}º número novamente: ") 
 
 
-    for j in range(1,4):
-      x:[int]=[0 for j in range(1,4)]
-      x:[j] == x:
-        print("Erro: número já digitado. Digite um número diferente.")
-         x = input(f"Digite o {i}º número novamente: ")
-        break
-    x = int(x)
+numero = input("Digite um número de até 4 dígitos: ")
 
-    x[j-1] = x
+
+if numero.isdigit():
+
+    if len(numero) <= 4:
+
+        numero = numero.zfill(4)
+
+        erro = False
+
+        for digito in numero:
+            if numero.count(digito) >= 3:
+                erro = True
+
+        if erro:
+            print("Erro: número com muitos dígitos repetidos.")
+
+        else:
+            contador = 0
+
+            while numero != "6174":
+
+                contador += 1
+
+                crescente = "".join(sorted(numero))
+                decrescente = "".join(sorted(numero, reverse=True))
+
+                resultado = int(decrescente) - int(crescente)
+
+                print(f"Iteração {contador}: {decrescente} - {crescente} = {resultado:04d}")
+
+                numero = str(resultado).zfill(4)
+
+            print("\nConstante de Kaprekar atingida!")
+
+    else:   
+        print("Erro: máximo 4 dígitos.")
+
+else:
+    print("Erro: digite apenas números inteiros positivos.")    
