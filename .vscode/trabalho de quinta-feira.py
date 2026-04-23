0c9304541bd47fd7fc8ace984580c813e264f87d
nathanielmendes02-lab <nathanielmendes02@email.com>
2026-04-22 23:46:27 -0300

    Add initial project files including Python scripts, C program, and VSCode configurations

---
+++ .vscode/trabalho de quinta-feira.py
@@ -0,0 +1,17 @@
+for i in range(1, 5):
+    x = input(f"Digite o {i}º número: ")
+    
+    while not x.isdigit():
+        print("Erro: digite apenas números inteiros positivos.") 
+        x = input(f"Digite o {i}º número novamente: ") 
+
+
+    for j in range(1,4):
+      x:[int]=[0 for j in range(1,4)]
+      x:[j] == x:
+        print("Erro: número já digitado. Digite um número diferente.")
+         x = input(f"Digite o {i}º número novamente: ")
+        break
+    x = int(x)
+
+    x[j-1] = x
---
+++ projeto1.py
@@ -0,0 +1,3 @@
+x = int(input("digite quatros numeros: "))
+if x < 0 or x == float('inf'): 
+    print("numero invalido")
